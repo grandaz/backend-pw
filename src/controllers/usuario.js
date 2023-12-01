fa333384edb6839ea6342a051f3776af45a152c2
Donayre <20180616@aloe.ulima.edu.pe>
2023-12-01 14:26:22 -0500

    Update login

--- src/controllers/usuario.js
+++ src/controllers/usuario.js
@@ -50,27 +50,46 @@ const remove = async (req,res) => {
 }
 
 const login = async (req, res) => {
-    try {
-      const { correo, contrasena } = req.body;
-  
-      // Valida si existe el usuario con el correo proporcionado
-      const usuario = await Usuario.findOne({ where: { correo } });
-  
-      if (!usuario) {
-        return res.status(404).json({ success: false, message: "Usuario no encontrado", data: null });
-      }
-  
-      // Valida la contraseña
-      if (contrasena !== usuario.contrasena) {
-        return res.status(400).json({ success: false, message: "Contraseña incorrecta", data: null });
-      }
-  
-      return res.status(200).json({ success: true, message: "Usuario logueado correctamente", data: null });
-    } catch (err) {
-      console.error(err);
-      return res.status(500).json({ success: false, message: "Error al loguear el usuario", data: null });
+  try {
+    const { correo, contrasena } = req.body;
+
+    // Valida si existe el usuario con el correo proporcionado
+    const usuario = await Usuario.findOne({ where: { correo } });
+
+    if (!usuario) {
+      return res.status(404).json({ success: false, message: "Usuario no encontrado", data: null });
     }
-  };
+
+    // Valida la contraseña
+    if (contrasena !== usuario.contrasena) {
+      return res.status(400).json({ success: false, message: "Contraseña incorrecta", data: null });
+    }
+
+    // Usuario logueado correctamente, devolver la información del usuario
+    return res.status(200).json({
+      success: true,
+      message: "Usuario logueado correctamente",
+      data: {
+        id: usuario.id,
+        nombre: usuario.nombre,
+        correo: usuario.correo,
+        fotoUrl: usuario.fotoUrl,
+        idTipoUsuario: usuario.idTipoUsuario,
+        idTipoDoc: usuario.idTipoDoc,
+        nroDoc: usuario.nroDoc,
+        contrasena: usuario.contrasena,
+        idioma: usuario.idioma,
+        prefijo: usuario.prefijo,
+        color: usuario.color
+        // Agrega más campos según sea necesario
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ success: false, message: "Error al loguear el usuario", data: null });
+  }
+};
+
   
   
   
